098df268fdaf6ad0f679932c0d8554b7f3833773
Documenter.jl <documenter@juliadocs.github.io>
2022-06-03 12:38:17 +0000

    build based on e66affe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = georectification","category":"page"},{"location":"#georectification","page":"Home","title":"georectification","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for georectification.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [georectification]","category":"page"}]
+}
